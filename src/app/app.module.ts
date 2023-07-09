f66e36c802363b5f5b184978500a5c6a1d3e136e
AbdallaFathy1121 <abdallafathy1023@gmail.com>
2023-07-09 23:14:43 +0300

    Add_Route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {path: '', component: Test1Component},
+  {path: 'test2', component: Test2Component}
+]
 
 @NgModule({
-  declarations: [		
+  declarations: [							
     AppComponent,
       CockpitComponent,
-      ServerElementComponent
+      ServerElementComponent,
+      Test1Component,
+      Test2Component
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

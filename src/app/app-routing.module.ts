8d94e9ca596801c13a7940ab491256773ff29d7b
AbdallaFathy1121 <abdallafathy1023@gmail.com>
2023-07-12 23:02:10 +0300

    Add_AppRoutingModule&NotFoundRoute

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+import { UsersComponent } from './users/users.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: '', component: Test1Component},
+  {path: 'test2', component: Test2Component},
+  {path: 'users', component: UsersComponent},
+  {path: 'cockpit', component: CockpitComponent},
+  {path: 'servers', component: ServerElementComponent},
+  {path: 'servers/:name/:content', component: ServerElementComponent},
+  {path: 'not-found', component: NotFoundComponent},
+  {path: '**', redirectTo: '/not-found'}
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,35 +8,26 @@ import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
 import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
-import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './users/user/user.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-
-const appRoutes: Routes = [
-  {path: '', component: Test1Component},
-  {path: 'test2', component: Test2Component},
-  {path: 'users', component: UsersComponent},
-  {path: 'cockpit', component: CockpitComponent},
-  {path: 'servers', component: ServerElementComponent},
-  {path: 'servers/:name/:content', component: ServerElementComponent},
-]
 
 @NgModule({
-  declarations: [								
+  declarations: [									
     AppComponent,
       CockpitComponent,
       ServerElementComponent,
       Test1Component,
       Test2Component,
       UsersComponent,
-      UserComponent
+      UserComponent,
+      NotFoundComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
